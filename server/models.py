ac99e883852bab9b37f39bd08b97cee5ac14c506
ndurushima <ndu1994@gmail.com>
2025-09-13 13:32:05 -0500

    built out models.py.

--- server/models.py
+++ server/models.py
@@ -51,5 +51,63 @@ class Recipe(db.Model):
 
 class MealPlan(db.Model):
     __tablename__ = "meal_plans"
-    id 
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    week_start = db.Column(db.Date, nullable=False)
+
+    items = db.relationship("shoppingItem", backref="meal_plan", cascade="all, delete-orphan")
+    shopping_items = db.relationship("shoppingItem", backref="meal_plan", cascade="all, delete-orphan")
+
+    def to_dict(self, include_items=False, include_shopping=False):
+        data = {
+            "id": self.id,
+            "user_id": self.user_id,
+            "week_Start": self.week_start.isoformat()
+        }
+        if include_items:
+            data["items"] = [items.to_dict() for items in self.items]
+        if include_shopping:
+            data["shopping_items"] = [shopping.to_dict() for shopping in self.shopping_items]
+        return data
+    
+
+class MealItem(db.Model):
+    __tablename__ = "meal_items"
+    id = db.Column(db.Integer, primary_key=True)
+    meal_plan_id = db.Column(db.Integer, db.ForeignKey("meal_plans.id"), nullable=False)
+    recipe_id = db.Column(db.Integer, db.ForeignKey("recipes.id"), nullable=True)
+    day = db.Column(db.String(10), nullable=False) 
+
+    # breakfast/lunch/dinner/snack
+    meal_type = db.Column(db.String(20), nullable=False) 
+    
+    recipe = db.relationship("Recipe")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "meal_plan_id": self.meal_plan_id,
+            "recipe_id": self.recipe_id,
+            "day": self.day,
+            "meal_type": self.meal_type,
+            "recipe": self.recipe.to_dict() if self.recipe else None
+        }
+    
+
+class ShoppingItem(db.Model):
+    __tablename__ = "shopping_items"
+    id = db.Column(db.Integer, primary_key=True)
+    meal_plan_id = db.Column(db.Integer, db.ForeignKey("meal_plans.id"), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.String(50), nullable=False)
+    checked = db.Column(db.Boolean, default=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "meal_plan_id": self.meal_plan_id,
+            "name": self.name,
+            "quantity": self.quantity,
+            "checked": self.checked
+        }
 
