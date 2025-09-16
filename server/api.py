c336ed33db1230feed66f46f059343ec7612f2d0
ndurushima <ndu1994@gmail.com>
2025-09-15 21:29:33 -0500

    fixed import issues

--- server/api.py
+++ server/api.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from . import db
+from .extensions import db
 from .models import Recipe, MealPlan, MealItem, ShoppingItem, User
 from .pagination import paginate
 
--- server/auth.py
+++ server/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from . import db
+from .extensions import db, bcrypt
 from .models import User
 
 auth_bp = Blueprint("auth", __name__)
