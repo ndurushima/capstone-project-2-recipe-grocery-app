55d8b108f742aade8522f5a75ec4a807742d9d33
ndurushima <ndu1994@gmail.com>
2025-09-15 21:11:31 -0500

    added meal items and shopping items to api.py

--- server/api.py
+++ server/api.py
@@ -48,13 +48,14 @@ def update_recipe(recipe_id):
     db.session.commit()
     return recipe.to_dict(), 200
 
+
 # -------- Meal Plans --------
 @api_bp.get("/meal_plans")
 @jwt_required()
 def list_meal_plans():
     uid = get_jwt_identity()
-    q = MealPlan.query.filter_by(user_id=uid).order_by(MealPlan.week_start.desc())
-    return paginate(q, request, serializer=lambda mp: mp.to_dict())
+    query = MealPlan.query.filter_by(user_id=uid).order_by(MealPlan.week_start.desc())
+    return paginate(query, request, serializer=lambda meal_plan: meal_plan.to_dict())
 
 
 @api_bp.post("/meal_plans")
@@ -63,25 +64,114 @@ def create_meal_plan():
     uid = get_jwt_identity()
     data = request.get_json() or {}
     week_start = data.get("week_start") # ISO date string
-    mp = MealPlan(user_id=uid, week_start=week_start)
-    db.session.add(mp)
+    meal_plan = MealPlan(user_id=uid, week_start=week_start)
+    db.session.add(meal_plan)
     db.session.commit()
-    return mp.to_dict(), 201
+    return meal_plan.to_dict(), 201
 
 
 @api_bp.get("/meal_plans/<int:mpid>")
 @jwt_required()
-def get_meal_plan(mpid):
+def get_meal_plan(meal_plan_id):
     uid = get_jwt_identity()
-    mp = MealPlan.query.filter_by(id=mpid, user_id=uid).first_or_404()
-    return mp.to_dict(include_items=True, include_shopping=True)
+    meal_plan = MealPlan.query.filter_by(id=meal_plan_id, user_id=uid).first_or_404()
+    return meal_plan.to_dict(include_items=True, include_shopping=True)
 
 
 @api_bp.delete("/meal_plans/<int:mpid>")
 @jwt_required()
-def delete_meal_plan(mpid):
+def delete_meal_plan(meal_plan_id):
     uid = get_jwt_identity()
-    mp = MealPlan.query.filter_by(id=mpid, user_id=uid).first_or_404()
-    db.session.delete(mp)
+    meal_plan = MealPlan.query.filter_by(id=meal_plan_id, user_id=uid).first_or_404()
+    db.session.delete(meal_plan)
+    db.session.commit()
+    return {"ok": True}
+
+
+# -------- Meal Items --------
+@api_bp.post("/meal_items")
+@jwt_required()
+def create_meal_item():
+    uid = get_jwt_identity()
+    data = request.get_json() or {}
+    meal_plan = MealPlan.query.filter_by(id=data.get("meal_plan_id"), user_id=uid).first_or_404()
+    meal_item = MealItem(meal_plan_id=meal_plan.id, recipe_id=data.get("recipe_id"), day=data.get("day"), meal_type=data.get("meal_type"))
+    db.session.add(meal_item)
+    db.session.commit()
+    return meal_item.to_dict(), 201
+
+
+@api_bp.patch("/meal_items/<int:miid>")
+@jwt_required()
+def update_meal_item(miid):
+    uid = get_jwt_identity()
+    meal_item = MealItem.query.join(MealPlan).filter(MealItem.id==miid, MealPlan.user_id==uid).first_or_404()
+    data = request.get_json() or {}
+    if "recipe_id" in data:
+        meal_item.recipe_id = data["recipe_id"]
+    if "day" in data:
+        meal_item.day = data["day"]
+    if "meal_type" in data:
+        meal_item.meal_type = data["meal_type"]
+    db.session.commit()
+    return meal_item.to_dict()
+
+
+@api_bp.delete("/meal_items/<int:meal_item_id>")
+@jwt_required()
+def delete_meal_item(meal_item_id):
+    uid = get_jwt_identity()
+    meal_item = MealItem.query.join(MealPlan).filter(MealItem.id==meal_item_id, MealPlan.user_id==uid).first_or_404()
+    db.session.delete(meal_item)
+    db.session.commit()
+    return {"ok": True}
+
+
+# -------- Shopping Items --------
+@api_bp.get("/shopping_items")
+@jwt_required()
+def list_shopping_items():
+    uid = get_jwt_identity()
+    meal_plan_id = request.args.get("meal_plan_id", type=int)
+    query = ShoppingItem.query.join(MealPlan).filter(MealPlan.user_id==uid)
+    if meal_plan_id:
+        query = query.filter(ShoppingItem.meal_plan_id==meal_plan_id)
+    return paginate(query, request)
+
+
+@api_bp.post("/shopping_items")
+@jwt_required()
+def create_shopping_item():
+    uid = get_jwt_identity()
+    data = request.get_json() or {}
+    meal_plan = MealPlan.query.filter_by(id=data.get("meal_plan_id"), user_id=uid).first_or_404()
+    shopping_item = ShoppingItem(meal_plan_id=meal_plan.id, name=data.get("name"), quantity=data.get("quantity"))
+    db.session.add(shopping_item)
+    db.session.commit()
+    return shopping_item.to_dict(), 201
+
+
+@api_bp.patch("/shopping_items/<int:sid>")
+@jwt_required()
+def update_shopping_item(shopping_item_id):
+    uid = get_jwt_identity()
+    shopping_item = ShoppingItem.query.join(MealPlan).filter(ShoppingItem.id==shopping_item_id, MealPlan.user_id==uid).first_or_404()
+    data = request.get_json() or {}
+    if "name" in data:
+        shopping_item.name = data["name"]
+    if "quantity" in data:
+        shopping_item.quantity = data["quantity"]
+    if "checked" in data:
+        shopping_item.checked = data["checked"]
+    db.session.commit()
+    return shopping_item.to_dict()
+
+
+@api_bp.delete("/shopping_items/<int:sid>")
+@jwt_required()
+def delete_shopping_item(shopping_item_id):
+    uid = get_jwt_identity()
+    shopping_item = ShoppingItem.query.join(MealPlan).filter(ShoppingItem.id==shopping_item_id, MealPlan.user_id==uid).first_or_404()
+    db.session.delete(shopping_item)
     db.session.commit()
     return {"ok": True}
